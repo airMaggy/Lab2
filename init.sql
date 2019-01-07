9449d0cfb9e8e5721ad4f8e6945d747b2f27536d
Mapkn3 <79608236848@ya.ru>
2019-01-07 19:44:19 +0400

    Init commit

---
+++ init.sql
@@ -0,0 +1,113 @@
+INSERT INTO author VALUES (
+    0,
+    'Н.В. Гоголь',
+    1809,
+    1852,
+    'Великие Сорочинцы',
+    'Вий как образ жизни',
+    0
+);
+
+INSERT INTO author VALUES (
+    1,
+    'А.П. Чехов',
+    1860,
+    1940,
+    'Таганрог',
+    'Мой герой',
+    0
+);
+
+INSERT INTO author VALUES (
+    2,
+    'М.В. Семёнова',
+    1958,
+    0,
+    'Санкт-Петербург',
+    'Мы придём за Тобою, Ты дождись нас, Крылатый. Мы придём за тобою. Лишь не складывай крыл',
+    1
+);
+
+INSERT INTO author VALUES (
+    3,
+    'Борис Акунин',
+    1956,
+    0,
+    'Зестафони, Грузия',
+    'Он на самом деле Григорий Шавлвович Чхартшвили, это презабавно',
+    1
+);
+
+INSERT INTO author VALUES (
+    4,
+    'М.С. Ахмедова',
+    1955,
+    2013,
+    'Омск',
+    '',
+    0
+);
+
+INSERT INTO book VALUES (
+    0,
+    'Мёртвые души',
+    1842,
+    'Политика, Сатира',
+    207
+);
+
+INSERT INTO book VALUES (
+    1,
+    'Валькирия',
+    1996,
+    'Историческое фэнтези',
+    608
+);
+INSERT INTO book VALUES (
+    2,
+    'Волкодав. Самоцветные горы',
+    2003,
+    'Историческое фэнтези',
+    410
+);
+
+INSERT INTO book VALUES (
+    3,
+    'Ледяная Королева',
+    2010,
+    'Фэнтези',
+    762
+);
+
+INSERT INTO book VALUES (
+    4,
+    'Азазель',
+    1998,
+    'Исторический детектив',
+    322
+);
+
+INSERT INTO authors_for_book VALUES (
+    0,
+    0
+);
+
+INSERT INTO authors_for_book VALUES (
+    2,
+    1
+);
+
+INSERT INTO authors_for_book VALUES (
+    2,
+    2
+);
+
+INSERT INTO authors_for_book VALUES (
+    4,
+    3
+);
+
+INSERT INTO authors_for_book VALUES (
+    3,
+    4
+);
